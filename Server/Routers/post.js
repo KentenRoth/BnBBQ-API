4681d8c7741c24663dd858f420ac91dad1392bb5
Kent Roth <kentencroth@yahoo.com>
2021-02-03 17:24:25 -0600

    Get posts

--- Server/Routers/post.js
+++ Server/Routers/post.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = new express.Router();
 const Post = require('../Models/Post');
 
-router.post('/post', async (req, res) => {
+router.post('/posts', async (req, res) => {
 	const post = new Post(req.body);
 	try {
 		await post.save();
@@ -10,6 +10,16 @@ router.post('/post', async (req, res) => {
 	} catch (error) {
 		res.status(400).send(error);
 	}
+});
+
+router.get('/posts', (req, res) => {
+	Post.find({})
+		.then((posts) => {
+			res.send(posts);
+		})
+		.catch((error) => {
+			res.status(500).send();
+		});
 });
 
 module.exports = router;
