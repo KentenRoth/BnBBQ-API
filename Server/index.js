fc76ac067975b31a0717cb1666782f6cc7400380
Kent Roth <kentencroth@yahoo.com>
2021-02-03 15:52:32 -0600

    edited file name

--- Server/index.js
+++ Server/index.js
@@ -1,11 +1,13 @@
 require('./mongoose');
 
 const express = require('express');
+const beardPostRouter = require('./Routers/beardPost');
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+app.use(beardPostRouter);
 
 app.listen(port, () => {
 	console.log(`Server up on ${port}`);
