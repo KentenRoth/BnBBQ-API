3188ca8b05da102b50b1d53876d7e7e984949060
Kent Roth <kentencroth@yahoo.com>
2021-02-08 12:37:43 -0600

    Setup auth file.

--- Server/Middleware/auth.js
+++ Server/Middleware/auth.js
@@ -1,6 +1,24 @@
 const jwt = require('jsonwebtoken');
 const User = require('../Models/User');
 
-const auth = (req, res, next) => {};
+const auth = async (req, res, next) => {
+	try {
+		const token = req.header('Authorization').replace('Bearer ', '');
+		const decoded = jwt.verify(token, process.env.HIDDEN_SENTENCE);
+		const user = await User.findOne({
+			_id: decoded._id,
+			'tokens.token': token,
+		});
+
+		if (!user) {
+			throw new Error();
+		}
+
+		req.user = user;
+		next();
+	} catch (error) {
+		res.status(401).send({ error: 'Not Authenticated' });
+	}
+};
 
 module.exports = auth;
