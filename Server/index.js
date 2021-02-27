c356614332ba4762f4f2e623ac7f27157dfdc015
Kent Roth <kentencroth@yahoo.com>
2021-02-27 08:14:56 -0600

    fixing CORS issue

--- Server/index.js
+++ Server/index.js
@@ -7,6 +7,19 @@ const userRouter = require('./Routers/user');
 const app = express();
 const port = process.env.PORT;
 
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', 'localhost:3000');
+	res.header(
+		'Access-Control-Allow-Methods',
+		'GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE'
+	);
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	);
+	next();
+});
+
 app.use(express.json());
 app.use(postRouter);
 app.use(userRouter);
