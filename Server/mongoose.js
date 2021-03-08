bfd42bb52b33915cb7c6e2ffcb10df062fd2ca72
Kent Roth <kentencroth@yahoo.com>
2021-03-08 14:10:36 -0600

    cors

--- Server/mongoose.js
+++ Server/mongoose.js
@@ -1,9 +1,13 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URL, {
-	useNewUrlParser: true,
-	useCreateIndex: true,
-	useUnifiedTopology: true,
-	useFindAndModify: false,
-});
+mongoose.connect(
+	process.env.MONGODB_URL,
+	{
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+	},
+	console.log('MongoDB Connected')
+);
